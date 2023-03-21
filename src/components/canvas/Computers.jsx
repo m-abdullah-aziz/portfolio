fc8f231c2c383abd1dbf5683fc92a56b2ed270ed
Muhammad Abdullah Aziz <muhammadabdullahaziz@outlook.com>
2023-03-21 05:00:51 +0500

    moving back to gltf model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,7 +6,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.glb");
+  const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black"/>
