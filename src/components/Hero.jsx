f50e55ac02d4fea2614b68fdc55eae7f83de1b5b
Muhammad Abdullah Aziz <muhammadabdullahaziz@outlook.com>
2023-03-27 03:31:47 +0500

    downward arrow animation added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
       <div className={ `${styles.paddingX} absolute inset-0
-        top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        top-[115px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
           <div className='flex flex-col justify-center items-center mt-5'>
             <div className=' w-5 h-5 rounded-full bg-[#915eff]' />
             <div className='w-1 sm:h-80 h-40 violet-gradient' />
@@ -33,22 +33,13 @@ const Hero = () => {
           </div>
         </div>
         <ComputersCanvas />
-        <div className='absolute sm:bottom-6 bottom-10 w-full 
+        <div className='absolute sm:bottom-14 bottom-16 w-full 
          flex justify-center items-center'>
             <a href="#about">
-              <div className='w-[35px] h-[64px] rounded-3xl border-4 
-                border-secondary flex justify center items-start p-2'>
-                <motion.div 
-                  animate={{
-                    y:[0,23,0]
-                  }}
-                  transition={{
-                    duration:1.5,
-                    repeat: Infinity,
-                    repeatType: 'loop'
-                  }}
-                  className="w-3 h-3 rounded-full bg-secondary mb-1"
-                />
+              <div className='indicator'>
+                <span></span>
+                <span></span>
+                <span></span>
               </div>
             </a>
         </div>
