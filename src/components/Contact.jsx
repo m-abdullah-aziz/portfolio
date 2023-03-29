3ac13942854edb533bef9539658494ee247ab229
Muhammad Abdullah Aziz <muhammadabdullahaziz@outlook.com>
2023-03-29 19:58:42 +0500

    added new globe model

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,6 @@ import { motion } from 'framer-motion';
 import emailjs from  "@emailjs/browser";
 
 import { styles } from '../styles';
-import { EarthCanvas } from './canvas';
 import { slideIn } from '../utils/motion';
 import { SectionWrapper } from '../hoc';
 
@@ -48,7 +47,7 @@ const Contact = () => {
   }
 
   return (
-    <div className='xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 
+    <div className='xl:mt-12 flex xl:flex-row flex-col gap-10 
       overflow-hidden'>
        <motion.div
         variants={slideIn('left', "tween", 0.2, 1)}
@@ -95,6 +94,14 @@ const Contact = () => {
               </button>
           </form>
 
+       </motion.div>
+
+       <motion.div
+          variants={slideIn('right', "tween", 0.2, 1)}
+          className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px] 
+          flex'
+       >
+        <img src='./globe.png' className='m-auto'/>
        </motion.div>
     </div>
   )
