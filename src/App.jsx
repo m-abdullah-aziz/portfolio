c37c50f3c25bfb9b9cc4f09049406c2607704bcc
Muhammad Abdullah Aziz <muhammadabdullahaziz@outlook.com>
2023-03-29 10:40:55 +0500

    added back stars

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ const App = () => {
           */}
           <div className='relative z-0'>
             <Contact />
+            <StarsCanvas />
           </div>
       </div>
       </BrowserRouter>
